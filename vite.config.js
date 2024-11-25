b6e5c5f3e225e9a127e3f68354535cf8e2ea5749
Cory Zue <cory@coryzue.com>
2024-11-25 13:48:11 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This adds a global process.env object which prevents errors
+    'process.env': {},
+  },
   build: {
     lib: {
       entry: 'src/main.jsx',
