cd16a11504a01e5788a98d1026e2f777dce5a30b
Cory Zue <cory@coryzue.com>
2024-12-04 10:09:03 +0200

    make a central api

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,23 @@ import { CommentSection } from './CommentSection'
 import { Filters } from './CommentFilters'
 import type { CommentOptions } from './types'
 
-export { Filters }  // Export filters directly
+const BlueskyComments = {
+  init: function(elementId: string, options: CommentOptions) {
+    const element = document.getElementById(elementId)
+    if (!element) return;
 
-export function initBlueskyComments(elementId: string, options: CommentOptions) {
-  const element = document.getElementById(elementId)
-  if (!element) return;
-
-  ReactDOM.createRoot(element).render(
-    <React.StrictMode>
-      <CommentSection
-        uri={options.uri}
-        author={options.author}
-        onEmpty={options.onEmpty}
-        commentFilters={options.commentFilters}
-      />
-    </React.StrictMode>
-  )
+    ReactDOM.createRoot(element).render(
+      <React.StrictMode>
+        <CommentSection
+          uri={options.uri}
+          author={options.author}
+          onEmpty={options.onEmpty}
+          commentFilters={options.commentFilters}
+        />
+      </React.StrictMode>
+    )
+  },
+  Filters
 }
+
+export default BlueskyComments
