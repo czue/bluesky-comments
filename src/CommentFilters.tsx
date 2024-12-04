fefa28d1e5ef4ed0784a6c5f084501f7e44fc370
Cory Zue <cory@coryzue.com>
2024-12-04 09:28:49 +0200

    add no pins and exact match filters

--- src/CommentFilters.tsx
+++ src/CommentFilters.tsx
@@ -12,11 +12,19 @@ const MinCharacterCountFilter = (min: number): (comment: AppBskyFeedDefs.ThreadV
   }
 }
 
+const ExactMatchFilter = (text: string): (comment: AppBskyFeedDefs.ThreadViewPost) => boolean => {
+  return (comment: AppBskyFeedDefs.ThreadViewPost) => {
+    return comment.post.record.text === text;
+  }
+}
+
 export const Filters = {
   MinLikeCountFilter,
   MinCharacterCountFilter,
+  ExactMatchFilter,
   NoLikes: MinLikeCountFilter(0),
   TooShort: MinCharacterCountFilter(5),
+  NoPins: ExactMatchFilter('📌'),
 };
 
 export default Filters;
